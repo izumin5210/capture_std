94a9234c14a6fb1d2da39b09d3d14f669c9348cf
izumin5210 <masayuki@izumin.info>
2014-05-04 18:29:02 +0900

    Add capture_std_spec

--- spec/capture_std_spec.rb
+++ spec/capture_std_spec.rb
@@ -5,7 +5,30 @@ describe CaptureStd do
     CaptureStd::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should eq(true)
+  describe '#capture' do
+    let(:msg) { "test message for stream output"}
+    context 'w/o parameter' do
+      it 'return stdout' do
+        expect(CaptureStd.capture { print msg }).to eq msg
+      end
+    end
+
+    context 'w/ :stdout' do
+      it 'return stdout' do
+        expect(CaptureStd.capture(:stdout) { print msg }).to eq msg
+      end
+    end
+
+    context 'w/ :stderr' do
+      it 'return stderr' do
+        expect(CaptureStd.capture(:stderr) { warn msg }).to eq "#{msg}\n"
+      end
+    end
+
+    context 'w/ invalid parameter' do
+      it 'raise TypeError' do
+        expect{CaptureStd.capture(:foobar) { print msg }}.to raise_error(TypeError, 'Invalid parameter')
+      end
+    end
   end
 end
