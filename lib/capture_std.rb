11cfd36f458624a47a21172e0a25d943d42b1d12
izumin5210 <masayuki@izumin.info>
2014-05-04 18:37:36 +0900

    Implement CaptureStd.capture

--- lib/capture_std.rb
+++ lib/capture_std.rb
@@ -1,5 +1,22 @@
 require "capture_std/version"
+require "tempfile"
 
 module CaptureStd
-  # Your code goes here...
+  def self.capture(stream=:stdout)
+    captured_stream = Tempfile.new(stream.to_s)
+    io = eval("$#{stream}")
+    origin_io = io.dup
+    io.reopen(captured_stream)
+
+    yield
+
+    io.rewind
+    captured_stream.read
+  rescue TypeError
+    raise TypeError,"Invalid parameter"
+  ensure
+    captured_stream.close
+    captured_stream.unlink
+    io.reopen(origin_io) unless origin_io.nil?
+  end
 end
